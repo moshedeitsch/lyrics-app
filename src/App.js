1384b8eba0a5f1f60a5e0c4ef20b54cc5c652b4a
Moshe Deitsch <me@moshedeitsch.com>
2020-01-26 18:19:40 +0200

    installing moment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Index from './components/layout/Index'
+import Lyrics from './components/tracks/Lyrics'
+import { ContextController } from "./context";
+
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-        Hello
-    </div>
+    <ContextController>
+      <Router>
+        <>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </ContextController>
   );
-}
+};
 
 export default App;
